4067e5abc612253165c755005e6fac8d7a62fd71
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2024-11-04 12:06:33 +0530

    cart

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 export default function Cart() {
     const userId=localStorage.getItem("userId")
     const [cartItems,setCartItems]=useState([])
+    const [loading,setLoading]=useState(true)
+
     useEffect(()=>{
         fetchCartItems()
     },[])
@@ -12,18 +14,27 @@ export default function Cart() {
             .then((res)=>{
                 console.log(res.data.items)
                 setCartItems(res.data.items)
+                setLoading(false)
             })
     }
     return (
-        <div className="cart-container">
+        <div>
             {
-                cartItems.map((item)=>(
-                    <div className="productItem" key={item._id}>
-                        <p>Name:{item.product.name}</p>
-                        <p>Stock:{item.product.stock}</p>
-                        <p>Quantity:{item.quantity}</p>
-                    </div>
-                ))
+               loading?(
+                <p>Loading</p>
+               ):(
+                <div className="cart-container">
+                    {
+                         cartItems.map((item)=>(
+                            <div className="productItem" key={item._id}>
+                                <p>Name:{item.product.name}</p>
+                                <p>Stock:{item.product.stock}</p>
+                                <p>Quantity:{item.quantity}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+               )
             }
         </div>
     )
